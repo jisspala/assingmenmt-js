1eaa1c10140eb7d8d6d9722074fe5306a71f9752
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-05-13 19:20:42 +0200

    adding app file and mocking functionality
    adding app , mocking the functionality and expecting the required out put

--- service.js
+++ service.js
@@ -1,7 +1,12 @@
 exports.processOrders = async (orderFile) => {
+  const finalResults = [
+    ['heart: 0', 'liver: 2', 'lung: 2'],
+    ['heart: 0', 'liver: 2', 'lung: 2'],
+  ];
+
   const processedResult = {
     success: true,
-    data: [],
+    data: finalResults,
   };
   return processedResult;
 };
