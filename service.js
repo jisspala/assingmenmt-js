82d2dbde250c8cb60171e5276b67af441cc695a6
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-05-13 18:59:41 +0200

    adding basic tests
    configuring and adding basic tests for TDD

---
+++ service.js
@@ -0,0 +1,7 @@
+exports.processOrders = async (orderFile) => {
+  const processedResult = {
+    success: true,
+    data: [],
+  };
+  return processedResult;
+};
---
+++ tests/service.test.js
@@ -0,0 +1,13 @@
+const { processOrders } = require('../service');
+describe('processOrders', () => {
+  const filePath = 'dummy.csv';
+
+  test('should return correct result status', async () => {
+    const processedOrders = await processOrders(filePath);
+    expect(processedOrders.success).toBe(true);
+  });
+  test('should return correct processed orders result', async () => {
+    const processedOrders = await processOrders(filePath);
+    expect(processedOrders.data.length).toBeGreaterThan(0);
+  });
+});
