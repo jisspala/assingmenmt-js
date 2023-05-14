15da1ebb135d4a6deb0d942b5b7cbab5deb40c1d
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-05-14 23:26:59 +0200

    test: adding more tests
    adding more tests to cover more situations

--- tests/service.test.js
+++ tests/service.test.js
@@ -10,9 +10,28 @@ afterEach(() => {
 describe('processOrders', () => {
   test('should return correct result status and data', async () => {
     filePath = 'tests/test_assets/correct_orders.csv';
+    const expectedResult = [
+      ['heart: 0', 'liver: 2', 'lung: 2'],
+      ['heart: 4', 'liver: 0', 'lung: 0'],
+      ['heart: 0', 'liver: 0', 'lung: 1'],
+    ];
     const processedOrders = await processOrders(filePath);
     expect(processedOrders.success).toBe(true);
-    expect(processedOrders.data.length).toBeGreaterThan(0);
+    expect(processedOrders.data?.length).toBeGreaterThan(0);
+    expect(processedOrders.data).toEqual(expectedResult);
+  });
+
+  test('should return correct result status and data for the organ which dont have any bonus ', async () => {
+    filePath = 'tests/test_assets/correct_orders_new_organ.csv';
+    const expectedResult = [
+      ['heart: 0', 'liver: 2', 'lung: 2'],
+      ['heart: 0', 'liver: 0', 'lung: 0', 'neworgan: 3'],
+    ];
+
+    const processedOrders = await processOrders(filePath);
+    expect(processedOrders.success).toBe(true);
+    expect(processedOrders.data?.length).toBeGreaterThan(0);
+    expect(processedOrders.data).toEqual(expectedResult);
   });
 
   test('should return error message and status when order file is not exists', async () => {
@@ -31,6 +50,13 @@ describe('processOrders', () => {
 
   test('should return error message and status when order price is invalid', async () => {
     filePath = 'tests/test_assets/orders_invalid_price.csv';
+    const processedOrders = await processOrders(filePath);
+    expect(processedOrders.success).toBe(false);
+    expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
+  });
+
+  test('should return error message and status when order file is empty', async () => {
+    filePath = 'tests/test_assets/no_data.csv';
     const processedOrders = await processOrders(filePath);
     expect(processedOrders.success).toBe(false);
     expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
